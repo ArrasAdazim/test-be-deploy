87dec022ca37c5d26da8c383865d31a3183a4f81
ArrasAdazim <arasaladzim98@gmail.com>
2023-12-23 10:34:41 +0700

    add env

--- knexfile.ts
+++ knexfile.ts
@@ -7,9 +7,11 @@ const config: { [key: string]: Knex.Config } = {
   development: {
     client: process.env.DB_CLIENT,
     connection: {
+      host: process.env.DB_HOST,
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
+      port: 19482,
     },
     pool: {
       min: 2,
